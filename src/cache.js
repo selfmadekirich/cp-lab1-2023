fc7846b572677a7e52f8eb6f3e104526e58b3272
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2023-09-25 23:42:58 +0300

    add implementation

--- src/cache.js
+++ src/cache.js
@@ -1,22 +1,56 @@
 class Cache{
+
+   #storage 
+
    constructor(){
-    this.storage = {}
+    this.#storage = new Map()
+   }
+
+   #keyIsValid(key){
+    return !Array.isArray(key) && !(key instanceof Set) 
+            && !(key instanceof Map) && (key != "") && !(typeof key === 'object')
    }
 
    setValue(key,value,hits){
-    return undefined;
+
+    if (!this.#keyIsValid(key) || this.#storage.has(key) || hits < 1){
+        return false;
+    }
+
+    const hit = hits === undefined || hits === null ? 1 : hits
+    
+    this.#storage.set(key,{"data":value,"hits":hit})
+    return true;
+
    }
 
-   getValue(key){
-    return undefined;
+    getValue(key){
+    if (!this.#keyIsValid(key) || !this.#storage.has(key)){
+        return null;
+    }
+    this.#storage.get(key)['hits']--;
+
+    const data = this.#storage.get(key)['data']
+
+    if(this.#storage.get(key)['hits'] === 0){
+        this.#storage.get(key)['data'] = null
+    }
+    return data
    }
 
    getStats(){
-    return {}
+    const stats = [] 
+    this.#storage.forEach((value,key,map)=>{
+       stats.push({"key":key,"value": value['data'],"hit remains":value['hits']})
+    })
+    return stats
    }
 
    getStatByKey(key){
-    return {}
+    if (!this.#keyIsValid(key) || !this.#storage.has(key)){
+        return null;
+    }
+    return {"key":key,"value":this.#storage.get(key)['data'],"hit remains":this.#storage.get(key)['hits']}
    }
 
 }
--- test/cache.test.js
+++ test/cache.test.js
@@ -3,74 +3,74 @@ import {Cache} from "../src/cache";
 
 test('can set and get not-empty key,value,hit', () => {
     const c = new Cache()
-    result = c.setValue("validKey",123,10)
+    const result = c.setValue("validKey",123,10)
     expect(result).toBe(true);
     expect(c.getValue("validKey")).toBe(123);
 });
 
 test('can set and get not-empty key,value and null hit', () => {
     const c = new Cache()
-    result = c.setValue("validKey",1)
+    const result = c.setValue("validKey",1)
     expect(result).toBe(true);
     expect(c.getValue("validKey")).toBe(1);
 });
 
 test('can not set value with empty-string key', () => {
     const c = new Cache()
-    result = c.setValue("",1)
+    const result = c.setValue("",1)
     expect(result).toBe(false);
 });
 
 test('can not set value with array key', () => {
     const c = new Cache()
-    result = c.setValue([1,2,3,4],1)
+    const result = c.setValue([1,2,3,4],1)
     expect(result).toBe(false);
 });
 
 test('can not set value with  dict-key', () => {
     const c = new Cache()
-    result = c.setValue({"key":"value1"},1)
+    const result = c.setValue({"key":"value1"},1)
     expect(result).toBe(false);
 });
 
 
 test('can not set value with negative hit', () => {
     const c = new Cache()
-    result = c.setValue("validKey",-100)
+    const result = c.setValue("validKey","value",-100)
     expect(result).toBe(false);
 });
 
 test('can not set value with zero hit', () => {
     const c = new Cache()
-    result = c.setValue("validKey",0)
+    const result = c.setValue("validKey","value",0)
     expect(result).toBe(false);
 });
 
 test('can set and get string value', () => {
     const c = new Cache()
-    result = c.setValue("validKey","string")
+    const result = c.setValue("validKey","string")
     expect(result).toBe(true);
     expect(c.getValue("validKey")).toBe("string");
 });
 
 test('can set and get array value', () => {
     const c = new Cache()
-    result = c.setValue("validKey",[1,2,3,4,5])
+    const result = c.setValue("validKey",[1,2,3,4,5])
     expect(result).toBe(true);
-    expect(c.getValue("validKey")).toBe([1,2,3,4,5]);
+    expect(c.getValue("validKey")).toStrictEqual([1,2,3,4,5]);
 });
 
 test('can set and get dict value', () => {
     const c = new Cache()
-    result = c.setValue("validKey",{"key1":"value1","key2":"value2"})
+    const result = c.setValue("validKey",{"key1":"value1","key2":"value2"})
     expect(result).toBe(true);
-    expect(c.getValue("validKey")).toBe({"key1":"value1","key2":"value2"});
+    expect(c.getValue("validKey")).toStrictEqual({"key1":"value1","key2":"value2"});
 });
 
 
 test('can set and get value with integer key', () => {
     const c = new Cache()
-    result = c.setValue(123,"123")
+    const result = c.setValue(123,"123")
     expect(result).toBe(true);
     expect(c.getValue(123)).toBe("123");
 });
@@ -78,7 +78,7 @@ test('can set and get value with integer key', () => {
 
 test('can set and get value with negative numeric key', () => {
     const c = new Cache()
-    result = c.setValue(123.456,"123")
+    const result = c.setValue(123.456,"123")
     expect(result).toBe(true);
     expect(c.getValue(123.456)).toBe("123");
 });
@@ -90,22 +90,22 @@ test('return null if key is not present in Cache', () => {
 
 test('getStatByKey returns actual stats for key', () => {
     const c = new Cache()
-    result = c.setValue(123,"123",1203)
+    const result = c.setValue(123,"123",1203)
     expect(result).toBe(true);
-    expect(c.getStatByKey(123)).toBe({"key":123,"value":"123","hit remains":1203});
+    expect(c.getStatByKey(123)).toStrictEqual({"key":123,"value":"123","hit remains":1203});
 });
 
 test('every getValue dicreases hit number', () => {
     const c = new Cache()
-    result = c.setValue(123,"123",2)
+    const result = c.setValue(123,"123",2)
     expect(result).toBe(true);
     expect(c.getValue(123)).toBe("123");
-    expect(c.getStatByKey(123)).toBe({"key":123,"value":"123","hit remains":1});
+    expect(c.getStatByKey(123)).toStrictEqual({"key":123,"value":"123","hit remains":1});
 });
 
 test('return null if hit is zero', () => {
     const c = new Cache()
-    result = c.setValue(123,"123")
+    const result = c.setValue(123,"123")
     expect(result).toBe(true);
     expect(c.getValue(123)).toBe("123");
     expect(c.getValue(123)).toBe(null);
@@ -117,7 +117,7 @@ test('getStats returns actual info', () => {
     c.setValue(1,1,100)
     c.setValue(34.22,133.11,2)
     c.setValue("string","hello",20)
-    expect(c.getStats()).toBe(
+    expect(c.getStats()).toStrictEqual(
         [
             {"key":123,"value":"123","hit remains":3},
             {"key":1,"value":1,"hit remains":100},
@@ -126,15 +126,21 @@ test('getStats returns actual info', () => {
         ]);
 });
 
+test('Can not get stats from non-existing key', () => {
+    const c = new Cache()
+    expect(c.getStatByKey("non-existing")).toBe(
+        null);
+});
+
 test('if hit is zero , record presented in Stats', () => {
     const c = new Cache()
     c.setValue(123,"123")
     c.setValue(1,1)
     expect(c.getValue(1)).toBe(1);
-    expect(c.getStats()).toBe(
+    expect(c.getStats()).toStrictEqual(
         [
         {"key":123,"value":"123","hit remains":1},
-        {"key":1,"value":1,"hit remains":0}
+        {"key":1,"value":null,"hit remains":0}
     ]);
 });
 
