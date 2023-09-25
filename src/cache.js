34d866bc5df635f58f004d590d90ebdf6c552342
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2023-09-25 22:00:16 +0300

    funcs interface done

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,23 @@
 class Cache{
+   constructor(){
+    this.storage = {}
+   }
+
+   setValue(key,value,hits){
+    return false;
+   }
+
+   getValue(key){
+    return null;
+   }
+
+   getStats(){
+    return {}
+   }
+
+   getStatByKey(key){
+    return {}
+   }
 
 }
 export {Cache}
